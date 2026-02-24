7d94ad5419f4c66f2da94974004a17f5726de6fb
Jacob Dykstra <dykstraj@my.lanecc.edu>
2026-02-23 16:44:40 -0800

    Add project files.

---
+++ HangMan/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using HangMan.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangMan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
